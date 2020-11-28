119c7441fd341f1db47c3bdcd3bc9c29c4569011
Honganh2910 <75011270+Honganh2910@users.noreply.github.com>
2020-11-28 17:44:36 +0700

    Add files via upload

---
+++ testAPICheckNewVersion-master/src/com/company/ResponseCheckNewVersion.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResponseCheckNewVersion {
+    public String code;
+    public String message;
+    ArrayList<String> arr = new ArrayList<>();
+    String[] data = new String[0];
+
+}
